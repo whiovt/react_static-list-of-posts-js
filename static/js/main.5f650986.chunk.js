6b0df202b3d276bbd77a22f871e2e65c95743444
Anna Kovnatska <anna0725k@gmail.com>
2023-10-05 18:10:50 -0500

    Updates

---
+++ static/js/main.5f650986.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact_static-list-of-posts-js"]=this["webpackJsonpreact_static-list-of-posts-js"]||[]).push([[0],{10:function(t,s,e){"use strict";e.r(s);var c=e(3),i=e.n(c),n=(e(8),e(0)),o=function(){return Object(n.jsx)("section",{className:"App",children:Object(n.jsx)("h1",{className:"App__title",children:"Static list of posts"})})};i.a.render(Object(n.jsx)(o,{}),document.getElementById("root"))},8:function(t,s,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.5f650986.chunk.js.map
